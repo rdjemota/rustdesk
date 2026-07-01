f016787277fe9edadc2259bbf45a87ce71d28ddc
rdjemota <rd.jemota@softjem.com.br>
2026-07-01 10:20:07 -0300

    Init

--- res/msi/preprocess.py
+++ res/msi/preprocess.py
@@ -73,7 +73,7 @@ def make_parser():
         help='Connection type, e.g. "incoming", "outgoing". Default is empty, means incoming-outgoing',
     )
     parser.add_argument(
-        "--app-name", type=str, default="RustDesk", help="The app name."
+        "--app-name", type=str, default="IPMRmt", help="The app name."
     )
     parser.add_argument(
         "-v", "--version", type=str, default="", help="The app version."
@@ -85,7 +85,7 @@ def make_parser():
         "-m",
         "--manufacturer",
         type=str,
-        default="Purslane Tech Pte. Ltd.",
+        default="SoftJem Tecnologia",
         help="The app manufacturer.",
     )
     return parser
@@ -499,7 +499,7 @@ def update_license_file(app_name):
         license_content = f.read()
     license_content = license_content.replace("website rustdesk.com and other ", "")
     license_content = license_content.replace("RustDesk", app_name)
-    license_content = re.sub(r"Purslane(?: Tech Pte\.)? Ltd", app_name, license_content, flags=re.IGNORECASE)
+    license_content = re.sub("SoftJem Tecnologia", app_name, license_content, flags=re.IGNORECASE)
     with open(license_file, "w", encoding="utf-8") as f:
         f.write(license_content)
 
--- src/main.rs
+++ src/main.rs
@@ -48,8 +48,8 @@ fn main() {
     );
     let matches = App::new("rustdesk")
         .version(crate::VERSION)
-        .author("Purslane Tech Pte. Ltd.<info@rustdesk.com>")
-        .about("RustDesk command line tool")
+        .author("SoftJem Tecnologia<contact@softjem.com.br>")
+        .about("IPMRmt command line tool")
         .args_from_usage(&args)
         .get_matches();
     use hbb_common::{config::LocalConfig, env_logger::*};
